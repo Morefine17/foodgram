94afeacd3e1cf28d04b60b0a83a67244ff5f6b45
Morefine17 <Morefine17@yandex.ru>
2022-06-29 21:21:03 +0300

    review6

--- backend/foodgram_app/settings.py
+++ backend/foodgram_app/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default="secret_key")
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(' ')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split()
 
 # Application definitionn
 
