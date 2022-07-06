62f0294542dd3fa5e4a76161b90cdb1e999dda48
Morefine17 <Morefine17@yandex.ru>
2022-07-06 10:51:12 +0300

    Deploy

--- backend/foodgram_app/settings.py
+++ backend/foodgram_app/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default="secret_key")
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default="51.250.111.226").split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default="pelmewka.hopto.org").split(' ')
 
 
 # Application definitionn
